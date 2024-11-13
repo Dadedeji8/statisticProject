0eaab3338b3b8cecc0e5c1976e80fd329210c6bb
Dadedeji8 <97545459+Dadedeji8@users.noreply.github.com>
2024-11-13 10:29:13 +0100

    Update Calculator.jsx

--- src/Pages/Calculator.jsx
+++ src/Pages/Calculator.jsx
@@ -285,7 +285,7 @@ const Calculator = () => {
         return nums[middle].toFixed(2);
     };
     const calculateStandardDeviation = (values) => {
-        const numbers = values.map(Number);
+        const numbers = values.split(',').map(Number);
         const mean = numbers.reduce((acc, num) => acc + num, 0) / numbers.length;
         const variance = numbers.reduce((acc, num) => acc + Math.pow(num - mean, 2), 0) / numbers.length;
         return Math.sqrt(variance);
